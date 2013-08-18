8cb4c5b3ceb027f5526a1ffa5445f4927ef96eca
diegoachury <tiran_o@hotmail.com>
2013-08-18 17:12:26 -0500

    creando el repositorio de salmon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salmon::Application.config.secret_token = '8d95854cfb4df065adb69dbb6b03ec1da405cb1a4386ae34e489286541b742a126f9134c74cc09e2e17fb13fd4fa881051f5ae62f96f9e4151245251f4051318'
