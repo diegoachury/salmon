4d4d884d923e0d8cfa9178a1f3dabbc3c3b9caa8
diegoachury <tiran_o@hotmail.com>
2013-08-21 06:06:17 -0500

    se modifico el estilo de fondo de todas las vistas y se agrego el campo ficha tecnica a la tabla productos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820010722) do
+ActiveRecord::Schema.define(:version => 20130821005106) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(:version => 20130820010722) do
     t.integer  "category_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.text     "ficha_tecnica"
   end
 
   add_index "products", ["admin_id"], :name => "index_products_on_admin_id"
